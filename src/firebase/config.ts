002cf78f231079ae0f17ca84115c070cc8bc82f9
Noorul Isalam <graphispace123@gmail.com>
2026-02-03 07:02:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2747088117-667fe",
+  "appId": "1:332482340087:web:bce273e67da0f4970b9ff2",
+  "apiKey": "AIzaSyCrhxRYN-nZzQZ7SEBqNyMq-_FkujcxCSs",
+  "authDomain": "studio-2747088117-667fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "332482340087"
+};
